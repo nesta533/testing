13523db654ac01642e7058b800791fd75786543c
nesta533 <nesta533@hotmail.com>
2017-03-07 20:40:35 +0800

    Add HelloWorld to test actor construction

---
+++ testing/HelloWorld/GreetingActor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Akka.Actor;
+
+namespace HelloWorld
+{
+    public class GreetingActor : ReceiveActor
+    {
+        public GreetingActor()
+        {
+            Receive<Greet>( greet => { Console.WriteLine("Hello {0}", greet.Who); });
+        }
+        public class Greet
+        {
+            public Greet(string who)
+            {
+                Who = who;
+            }
+            public string Who{ get; private set; }
+        }
+
+    }
+}
