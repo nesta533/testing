e9d652ab35a41144bf24a512eba52c6c7e8534f2
awang3 <awang2@ra.rockwell.com>
2017-03-07 10:43:11 +0800

    Add EchoActor

---
+++ testing/EchoActorLib/EchoActor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace EchoActorLib
+{
+    public class EchoActor : ReceiveActor
+    {
+        public EchoActor()
+        {
+            Receive<Hello>(hello =>
+            {
+                Console.WriteLine("[{0}]: {1}", Sender, hello.Message);
+                Sender.Tell(hello);
+            });
+        }
+    }
+
+    public class Hello
+    {
+        public Hello(string message)
+        {
+            Message = message;
+        }
+        public string Message { get; private set; }
+    }
+}
