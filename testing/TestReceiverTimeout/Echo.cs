b8295acf5d8e81ac70c9642728a25f6b71d63ba2
awang3 <awang2@ra.rockwell.com>
2017-03-10 13:32:48 +0800

    test receive timeout

---
+++ testing/TestReceiverTimeout/Echo.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Actor;
+using System;
+
+
+namespace TestReceiverTimeout
+{
+    class Echo : ReceiveActor
+    {
+        public Echo()
+        {
+            Receive<string>( s => Console.WriteLine(s));
+
+            Receive<ReceiveTimeout>(timeout => Console.WriteLine(timeout.ToString()));
+
+            Context.SetReceiveTimeout(TimeSpan.FromSeconds(3.0));
+        }
+    }
+}
---
+++ testing/TestReceiverTimeout/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Akka.Actor;
+
+namespace TestReceiverTimeout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("system");
+
+            var echo = system.ActorOf<Echo>("echo");
+
+            // adjust the interval to see if can echo the timeout message.
+            TimeSpan interval = TimeSpan.FromSeconds(2.0);
+
+            for (var i = 0; i < 10; i++)
+            {
+                echo.Tell("Hello " + i.ToString());
+                Thread.Sleep(interval);      
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
