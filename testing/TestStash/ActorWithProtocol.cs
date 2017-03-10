cff820099cdcadb617e730e0d002fc0382c63ae6
awang3 <awang2@ra.rockwell.com>
2017-03-10 16:23:27 +0800

    test Stash

---
+++ testing/TestStash/ActorWithProtocol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Akka.Actor;
+namespace TestStash
+{
+    class ActorWithProtocol : UntypedActor, IWithUnboundedStash
+    {
+        public IStash Stash { get; set; }
+        protected override void OnReceive(object message)
+        {
+            if (message.Equals("open"))
+            {
+                Console.WriteLine(message);
+
+                BecomeStacked(m =>
+               {
+                   if (m.Equals("write"))
+                   {
+                       Console.WriteLine("Write");
+                   }
+                   else if (m.Equals("close"))
+                   {
+                       Stash.UnstashAll();
+                       UnbecomeStacked();
+                   }
+                   else
+                   {
+                       Stash.Stash();
+                   }
+               });
+            }
+        }
+    }
+}
---
+++ testing/TestStash/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace TestStash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("system");
+
+            var protocol = system.ActorOf<ActorWithProtocol>("protocol");
+
+            protocol.Tell("open");
+            protocol.Tell("write");
+
+            // stach the open message
+            for (var i = 0; i < 10; i++)
+            {
+                protocol.Tell("open");
+            }
+
+            protocol.Tell("close");
+
+            Console.ReadKey();
+        }
+    }
+}
