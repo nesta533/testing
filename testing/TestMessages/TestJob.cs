78c83ff465c909690faf70d12b1557eb74f39d70
nesta533 <nesta533@hotmail.com>
2017-03-13 22:43:37 +0800

    test messages define

---
+++ testing/TestMessages/TestJob.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMessages
+{
+    public class TestJob : IEquatable<TestJob>
+    {
+        public TestJob(string job)
+        {
+            Job = job;
+        }
+
+        public string Job { get; private set; }
+
+
+        public bool Equals(TestJob other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
