943952a1f4c6e13092ca90fb954bce5dbeed6dae
awang3 <awang2@ra.rockwell.com>
2017-03-14 17:19:19 +0800

    add example

---
+++ testing/TestMessages/TestJobResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMessages
+{
+    public class TestJobResult
+    {
+        public TestJobResult(TestJob job)
+        {
+            Job = job;
+        }
+
+        public TestJob Job { get; private set; }
+
+        public UInt32? NumberofTestCases { get; set; }
+
+    }
+}
