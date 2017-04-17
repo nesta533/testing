f7d79defa4c5231613581768d449350ee723b152
awang3 <awang2@ra.rockwell.com>
2017-04-17 14:25:08 +0800

    add comments for Thread Visible

--- testing/ThreadVisibleTesting/ThreadVisibleTesting.cpp
+++ testing/ThreadVisibleTesting/ThreadVisibleTesting.cpp
@@ -4,6 +4,24 @@
 #include <process.h>
 #include <Windows.h>
 
+/*
+Hi Eric,
+
+I has approved this code review. But after some thinking this noon, 
+I think that you need pay more attention HasBeenUpdated(), which will 
+access the member m_bHasBeenUpdated but not using any thread sync mechanism. 
+Please considering is it thread-safe?
+
+I also write a test program to verify whether the BOOL(typedef int BOOL) is visibility in multiple threads in Windows. 
+Visibility means that one thread modify someone shared variable, then other threads can see the modification right now. 
+The result is that BOOL is un-visible.  So, although you change the m_bHasBeenUpdated = FALSE, it is very possible that 
+other threads cannot see this change right now in some condition.
+
+The sample code is very easy. If the result of ++nRace++ can be reflected to other threads, then the expected result nRace == 20000. 
+But the actual result nRace is not equal 20000.
+
+*/
+
 const int THREAD_COUNT = 20;
 int nRace = 0;
 void Increase()
